e3fa48af72624be50c61ecb1ce0caffac1e58f16
Tim O'Brien <timo@t413.com>
2012-04-28 18:08:09 -0400

    Working Nordic communication. Added PI and position adjustment on the fly. Working on abstracting the position request more. Next will support mid-movement velocity matching.

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #define LED_ON		(PORTD |= (1<<6))
 #define LED_OFF		(PORTD &= ~(1<<6))
 #define LED_TOGGLE	(PORTD ^= (1<<6))
+#define FakePWM0(port,pin,val,period,incrcng_cntr) (port = ((incrcng_cntr %(period))>(val)) ? (port|(1<<(pin))) : (port&~(1<<(pin))))
 
 #define LED_CONFIG	(DDRD |= (1<<6))
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
@@ -26,9 +27,18 @@ struct servoMotion {
     uint16_t final_pos;    //request a position here, will be velocity controlled
     uint32_t pos_rqst_time;//time of requested position
     uint32_t move_duration;//time the requested transition takes to complete
+    float p,i;             //PID gains
     int16_t i_error;       //integral error
     int16_t last_val;      //for derivitive calculation
 };
+void newPosition(uint16_t goal, uint16_t duration, struct servoMotion * svo, uint32_t current_time);
+
+enum modes {
+    POSITION = 0,
+    ADJ_P, 
+    ADJ_I,
+};
+#define NUM_MODES 3
 
 int main(void) {
 
@@ -40,6 +50,7 @@ int main(void) {
 	pwm_init();
     timer0_init();
     init_spi();
+    configure_receiver();
     analogReference(ADC_REF_EXTERNAL);
     
     //for H-Bridge control lines: 
@@ -55,25 +66,26 @@ int main(void) {
     for (uint8_t m = 0; m < 6; m++) {  //loop through each svo instance and initialize it.
         svo[m].pid_goal_pos = svo[m].final_pos = svo[m].prev_pos = analogRead(m);
         svo[m].i_error = svo[m].pos_rqst_time = svo[m].last_val = svo[m].move_duration =  0;
+        svo[m].p = 20; svo[m].i = 2;
     } 
 
-	uint32_t lastCntl = 0, lastLED = 0, lastMove = 4000;
-    uint8_t lastMoveDir = 0;
+	uint32_t lastCntl = 0, lastPrint = 0, lastMove = 4000;
+    enum modes mode = POSITION;
+    uint8_t adj_servo = 0;
 	while (1) {
-        uint32_t current = millis();
+        uint32_t t_tics = tics();
+        uint32_t current = t_tics >> 11; //better than dividing by 2000 to get ms
         
         //periodic position requesting
         if ((current - lastMove) > 500) {
             for (uint8_t m = 0; m < 6; m++) { 
+                if (m==0 && (mode == POSITION)) { continue; }
                 if (current > (svo[m].pos_rqst_time + svo[m].move_duration + rand()%1000)) {
-                    svo[m].final_pos = rand()%850 + 100; //request a new position
-                    svo[m].prev_pos = svo[m].pid_goal_pos; //mark our current position
-                    svo[m].pos_rqst_time = current; //mark the current time
-                    svo[m].move_duration = rand()%6000 + 1000 + 2*abs(svo[m].final_pos - svo[m].prev_pos); //randomly set transition duration
+                    uint16_t dt = rand()%6000 + 1000 + 2*abs(svo[m].final_pos - svo[m].prev_pos);//transition duration
+                    newPosition(rand()%850 + 100, dt, &(svo[m]), current);
                 }
             }
             lastMove = current; 
-            lastMoveDir = !lastMoveDir;
         }
         
         //control system
@@ -101,17 +113,15 @@ int main(void) {
             //PID control
             for (uint8_t m = 0; m < 6; m++) {
                 int16_t error = ( analogRead(m) - (signed)svo[m].pid_goal_pos );
-                svo[m].i_error = limit( svo[m].i_error/2 + error, -200, 200);
-                write_motor(m, (error * 20) + (svo[m].i_error) );
+                svo[m].i_error = limit( svo[m].i_error/svo[m].i + error, -200, 200);
+                write_motor(m, (error * svo[m].p) + (svo[m].i_error) );
                 svo[m].last_val = error; //store last error
             }
             lastCntl = current;
         }
         
-        //led / debug-print control
-        if ((current - lastLED) > 50) { 
-            LED_TOGGLE; 
-            //printNumber((current),DEC);print(",");
+        //debug-print control
+        if ((current - lastPrint) > 50) { 
             printNumber(svo[0].pid_goal_pos,DEC); print(",");
             printNumber(svo[0].final_pos,DEC); print(",");
             for (uint8_t m = 0; m < 1; m++) {
@@ -120,12 +130,68 @@ int main(void) {
                 printNumber(a,DEC); print(",");
             }
             print("\n");
-            lastLED = current;
+            
+            lastPrint = current;
+        }
+        
+        //fancy LED output
+        {
+        #define pwm_steps 20
+        uint8_t led_clk_scl = 4-mode;
+        if (((current >> led_clk_scl)/pwm_steps)%2) FakePWM0(PORTD,6,(((current >> led_clk_scl))%pwm_steps),pwm_steps,t_tics); //fade in
+        else FakePWM0(PORTD,6,pwm_steps-(((current >> led_clk_scl))%pwm_steps),pwm_steps,t_tics); //fade out
+        }
+        
+        //Nordic fob read
+        if (!(PINE & (1<<6))) { //check Nordic module for data
+            uint8_t stat = rx_send_byte(0xFF);
+            if (stat & 0x40) { //We have data!  
+                uint8_t reci[4];
+                receive_data(reci);
+                
+                mode = (mode + (reci[0] == 0x0F)) % NUM_MODES; //center button
+                if (reci[0] == 0x0F) { print("mode = "); printNumber( mode,DEC ); print("\n"); }
+                
+                if (mode == POSITION) {
+                    int16_t dpos = (reci[0] == 0x1D)? 100:((reci[0] == 0x1E)? -200:0);
+                    
+                    newPosition(svo[0].final_pos + dpos, 1000, &(svo[0]), current);
+                }
+                else if (mode == ADJ_P) {
+                    if      (reci[0] == 0x1D) svo[0].p *= 1.2f; //UP
+                    else if (reci[0] == 0x1E) svo[0].p /= 1.2f; //DOWN
+                    print(" P="); printNumber( svo[0].p ,DEC ); print("\n");
+                }
+                else if (mode == ADJ_I) {
+                    if      (reci[0] == 0x1D) svo[0].i *= 1.2f; //UP
+                    else if (reci[0] == 0x1E) svo[0].i /= 1.2f; //DOWN
+                    print(" I="); printNumber( svo[0].i ,DEC ); print("\n");
+                }
+                
+                switch(reci[0]) {
+                    case 0x17: print("Left button"); break;
+                    case 0x1E: print("Bottom button"); break;
+                    case 0x1B: print("Right button"); break;
+                    case 0x1D: print("Top button"); break;
+                }
+                print(" Presses="); printNumber( reci[2],DEC ); print("\n");
+            }
         }
 
 	}
 }
 
+
+void newPosition(uint16_t goal, uint16_t duration, struct servoMotion * svo, uint32_t current_time) {
+    
+    //TODO: add velocity matching support..
+    // needs the diritive of the position function, solve for t, set start time accordingly.
+    goal = limit(goal, 100, 960); //limit incoming value.
+    svo->final_pos = goal;
+    svo->prev_pos = svo[0].pid_goal_pos;
+    svo->pos_rqst_time = current_time; //mark the current time
+    svo->move_duration = duration;
+}
 
 
 void write_motor(uint8_t motor, int16_t val) {
